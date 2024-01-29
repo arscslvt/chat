75b202b4e76664282082110c4b6d299e1f66109f
Salvatore Aresco <salvatorearesco2002@gmail.com>
2024-01-29 15:00:27 +0100

    [NEW] Metadata: Chat title when previewing a Chat link

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,6 +1,31 @@
+import { Metadata, ResolvingMetadata } from "next";
 import Home from "../page";
 import React from "react";
+import getThread from "@/lib/api/thread/get";
 
-export default function Chat() {
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.id;
+
+  // fetch data
+  const chat = await getThread(id).catch(() => null);
+
+  // optionally access and extend (rather than replace) parent metadata
+  // const previousImages = (await parent).openGraph?.images || []
+
+  return {
+    title: chat?.metadata.name || "A thread on Chat by Salvatore Aresco.",
+  };
+}
+
+export default async function Chat({ params, searchParams }: Props) {
   return <Home />;
 }
--- lib/api/thread/get.ts
+++ lib/api/thread/get.ts
@@ -1,7 +1,8 @@
 import { isLocal } from "@/lib/utils";
 import api from "../config";
+import { Thread } from "@/context/messages";
 
-export default async function getThread(id: string) {
+export default async function getThread(id: string): Promise<Thread> {
   const res = await api.get("/thread/" + id);
 
   isLocal && console.log(res.data);
