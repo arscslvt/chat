acb107e89c82c51a8fac2636032bb48bc7398c59
Salvatore Aresco <salvatorearesco2002@gmail.com>
2024-03-14 12:50:35 +0100

    [EDIT] Updated metadata on Maintenance page

--- app/maintenance/layout.tsx
+++ app/maintenance/layout.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Chat is under maintenance",
+  description:
+    "We're under maintenance. Chat servers are currently under maintenance. We are working hard to bring them back online.",
+};
 
 export default function MaintenanceLayout({
   children,
